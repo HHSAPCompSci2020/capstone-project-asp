f2f56c0e64da07dacf001bf988af00a98abb8254
Savio Esmailzadeh <sesmailzadeh652@student.fuhsd.org>
2021-05-03 17:04:18 -0700

    created classes

---
+++ MiniGolf/src/Player.java
@@ -0,0 +1,27 @@
+
+public class Player {
+
+	
+	
+//	public move(Card c, level l, PowerUp p , int dir){
+//		if(p != null){
+//		p.affect(c);
+//		}
+//		//power ups
+//
+//		//jump
+//		for(i = 0; i< jmomentum; i++){
+//		//move in the specified direction
+//		//check current tile
+//		//change behavior accordingly
+//		//ex. If current tile is a wall, don’t do anything
+//		}
+//		//move
+//		for(i = 0; i<momentum; i++){
+//		//move in the specified direction
+//		//check current tile
+//		//change behavior accordingly
+//		//ex. If current tile is a wall, change direction
+//		}
+
+}
