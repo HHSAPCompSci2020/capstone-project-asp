61663d5de80b9e1ae0ae4ae8a055b081da620b17
Ansel Liu <anselliu04@gmail.com>
2021-05-07 13:44:09 -0700

    alpha

--- MiniGolf/src/golf/game/LevelSelect.java
+++ MiniGolf/src/golf/game/LevelSelect.java
@@ -1,29 +1,37 @@
 package golf.game;
+
 import java.awt.Point;
 import java.awt.Rectangle;
+
+import g4p_controls.GImageButton;
+
 /**
  * 
  * @author Ansel
  *
  */
 
-public class LevelSelect extends Screen{
+public class LevelSelect extends Screen {
 
- 	private DrawingSurface surface;
+	private DrawingSurface surface;
 	private Rectangle level1;
 	private Rectangle back;
-	
+	private GImageButton level;
+	private String files[];
+
 	/*
-	 * Creates a new levelSelect 
+	 * Creates a new levelSelect
+	 * 
 	 * @param surface to draw on
 	 */
 	public LevelSelect(DrawingSurface drawingSurface) {
-		super (800, 800);
+		super(800, 800);
 		surface = drawingSurface;
-		
+		files = new String[] { "ballRED.png", "ballBLACK.png", "ballYELLOW.png" };
 		level1 = new Rectangle(300, 200, 200, 100);
 		back = new Rectangle(100, 100, 100, 100);
 		level1 = new Rectangle(400, 200, 200, 100);
+		level = new GImageButton(drawingSurface, 20, 14, files, "ballSWAG.png");
 	}
 
 	/*
@@ -31,41 +39,41 @@ public class LevelSelect extends Screen{
 	 */
 	public void draw() {
 
-        surface.pushStyle();
+		surface.pushStyle();
 
-        surface.background(255,255,255);
+		surface.background(255, 255, 255);
 
-        surface.rect(level1.x, level1.y, level1.width, level1.height, 10, 10, 10, 10);
-        surface.fill(0);
-        String str = "level 1";
-        float w = surface.textWidth(str);
-        surface.text(str, level1.x+level1.width/2-w/2, level1.y+level1.height/2);
-        
-        surface.noFill();
-        surface.rect(back.x, back.y, back.width, back.height, 10, 10, 10, 10);
-        surface.fill(0);
-        str = "Back";
-        w = surface.textWidth(str);
-        surface.text(str, back.x+back.width/2-w/2, back.y+back.height/2);
+		surface.rect(level1.x, level1.y, level1.width, level1.height, 10, 10, 10, 10);
+		surface.fill(0);
+		String str = "level 1";
+		float w = surface.textWidth(str);
+		surface.text(str, level1.x + level1.width / 2 - w / 2, level1.y + level1.height / 2);
 
-        surface.popStyle();
-    }
+		surface.noFill();
+		surface.rect(back.x, back.y, back.width, back.height, 10, 10, 10, 10);
+		surface.fill(0);
+		str = "Back";
+		w = surface.textWidth(str);
+		surface.text(str, back.x + back.width / 2 - w / 2, back.y + back.height / 2);
+
+		surface.popStyle();
+	}
 
 	/*
 	 * switches the screen if the level1 rectangle is clicked on
 	 */
-    public void mousePressed() {
-        Point p = surface.actualCoordinatesToAssumed(new Point(surface.mouseX,surface.mouseY));
-        if (level1.contains(p))
-            surface.switchScreen(ScreenSwitcher.SCREEN4);
-        if (back.contains(p))
-        	surface.switchScreen(ScreenSwitcher.SCREEN1);
-    }
-	
+	public void mousePressed() {
+		Point p = surface.actualCoordinatesToAssumed(new Point(surface.mouseX, surface.mouseY));
+		if (level1.contains(p))
+			surface.switchScreen(ScreenSwitcher.SCREEN4);
+		if (back.contains(p))
+			surface.switchScreen(ScreenSwitcher.SCREEN1);
+	}
+
 	@Override
 	public void switchScreen(int i) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
