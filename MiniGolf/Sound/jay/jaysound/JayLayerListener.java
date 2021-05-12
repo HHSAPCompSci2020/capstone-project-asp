ea38b7412b5a422af818aa46d5bbb68ec0af881a
Ansel Liu <anselliu04@gmail.com>
2021-05-11 19:38:42 -0700

    music

--- MiniGolf/Sound/jay/jaysound/JayLayerListener.java
+++ MiniGolf/Sound/jay/jaysound/JayLayerListener.java
@@ -2,9 +2,14 @@ package jay.jaysound;
 
 public interface JayLayerListener {
 
+	void playSong();
+
 	void songEnded();
+
 	void playlistEnded();
+
 	void musicStarted();
+
 	void musicStopped();
-	
+
 }
--- MiniGolf/src/golf/game/Music.java
+++ MiniGolf/src/golf/game/Music.java
@@ -3,7 +3,6 @@ package golf.game;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
 
@@ -12,8 +11,6 @@ import jay.jaysound.JayLayerListener;
 
 public class Music extends JPanel implements ActionListener, JayLayerListener {
 	private JComboBox<String> effects;
-	private JButton play, startstop, next;
-
 	private JayLayer sound;
 
 	public Music() {
@@ -37,16 +34,7 @@ public class Music extends JPanel implements ActionListener, JayLayerListener {
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		String source = arg0.getActionCommand();
-		if (source.equals("Play!")) {
-			int i = effects.getSelectedIndex();
-			if (i >= 0)
-				sound.playSoundEffect(i);
-		} else if (source.equals("Start") || source.equals("Next")) {
-			sound.nextSong();
-		} else if (source.equals("Stop")) {
-			sound.stopSong();
-		}
+		sound.nextSong();
 
 	}
 
@@ -71,12 +59,4 @@ public class Music extends JPanel implements ActionListener, JayLayerListener {
 	public void musicStopped() {
 
 	}
-
-	// As your program grows, you may want to...
-	// 1) Move this main method into its own 'main' class
-	// 2) Customize the JFrame by writing a class that extends it, then creating
-	// that type of object in your main method instead.
-	// 3) Rename this class (SimpleWindow is not a good name - this class actually
-	// represents the *Panel*, not the window)
-
 }
