17ba21397a7a6e9d50c74daafc5c55fdd129c4f4
Savio Esmailzadeh <sesmailzadeh652@student.fuhsd.org>
2021-05-11 20:47:24 -0700

    fixed player move method

--- MiniGolf/src/golf/game/Board.java
+++ MiniGolf/src/golf/game/Board.java
@@ -46,8 +46,6 @@ public class Board extends Screen{
 		back = new Rectangle(100, 100, 100, 100);
 		p = l.findPlayer();
 		
-		System.out.println(""+l.c.get(0).getMagnitude());
-		p.move(l.c.get(0), l, null, 1);
 	
 	}
 	
