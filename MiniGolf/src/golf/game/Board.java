c830805013677f5039732654c305085aa18fbca3
Prawat967 <65191555+Prawat967@users.noreply.github.com>
2021-05-15 16:37:02 -0700

    added powerups

--- MiniGolf/src/golf/game/Board.java
+++ MiniGolf/src/golf/game/Board.java
@@ -15,14 +15,16 @@ import java.util.ArrayList;
 public class Board extends Screen {
 	private Level l;
 	private Card cardSelected;
+	private PowerUp powerupSelected;
 	private boolean isMoving;
 	private DrawingSurface surface;
 	private Rectangle back;
 	private Rectangle back1;
-	public ArrayList<Rectangle2D.Float> cardMenu = new ArrayList<Rectangle2D.Float>();
+	private ArrayList<Rectangle2D.Float> cardMenu = new ArrayList<Rectangle2D.Float>();
 	private Player p;
 	private int level;
-	private boolean isCleared;
+	private ArrayList<Rectangle2D.Float> powerUpMenu = new ArrayList<Rectangle2D.Float>();
+	
 	private boolean[] cleared;
 
 	/**
@@ -51,11 +53,25 @@ public class Board extends Screen {
 		Music m = new Music();
 		m.actionPerformed(null);
 		instantiateCards();
-		isCleared = false;
+		instantiatePowerUps();
 		this.cleared = cleared;
 		System.out.println(cardMenu.size());
 
 		System.out.println(cardMenu.get(0).getCenterX());
+	}
+	
+	private void instantiatePowerUps() {
+		if(l.p.size()>0) {
+		float startx = DRAWING_WIDTH / 20;
+		float starty = DRAWING_HEIGHT * 9 / 10;
+
+		float change = (DRAWING_WIDTH - DRAWING_WIDTH / 20) / l.p.size();
+
+		for (int i = 0; i < l.p.size(); i++) {
+
+			this.powerUpMenu.add(new Rectangle2D.Float(change * i + startx, starty, 20, 20));
+		}
+		}
 	}
 
 	private void instantiateCards() {
@@ -81,6 +97,7 @@ public class Board extends Screen {
 		l.drawGrid(this, surface);
 		l.drawCard(this, surface);
 		p.draw(surface, this.DRAWING_WIDTH, this.DRAWING_HEIGHT);
+		l.drawPowerUps(this, surface);
 		surface.rect(back.x, back.y, back.width, back.height, 10, 10, 10, 10);
 		surface.rect(back1.x, back1.y, back1.width, back1.height);
 
@@ -94,36 +111,84 @@ public class Board extends Screen {
 
 		surface.fill(0, 255, 0);
 
-		if (cardSelected != null) {
+		if (cardSelected != null && powerupSelected!= null) {
 
 			if (surface.isPressed(KeyEvent.VK_LEFT)) {
+				powerupSelected.affect(cardSelected);
 				p.move(cardSelected, l, null, 4);
 
 				l.c.remove(cardSelected);
 				cardSelected = null;
+				powerupSelected = null;
 			}
 			if (surface.isPressed(KeyEvent.VK_RIGHT)) {
+				powerupSelected.affect(cardSelected);
 				p.move(cardSelected, l, null, 3);
 				System.out.println("this");
 				l.c.remove(cardSelected);
 				cardSelected = null;
+				
+				powerupSelected = null;
+			}
+
+			if (surface.isPressed(KeyEvent.VK_UP)) {
+				powerupSelected.affect(cardSelected);
+				p.move(cardSelected, l, null, 1);
+				
+				l.c.remove(cardSelected);
+				cardSelected = null;
+				powerupSelected = null;
+
+			}
+			if (surface.isPressed(KeyEvent.VK_DOWN)) {
+				powerupSelected.affect(cardSelected);
+				p.move(cardSelected, l, null, 2);
+				l.c.remove(cardSelected);
+				cardSelected = null;
+				powerupSelected = null;
+			}
+			
+		}
+		
+		if (cardSelected != null && powerupSelected== null) {
+
+			if (surface.isPressed(KeyEvent.VK_LEFT)) {
+				
+				p.move(cardSelected, l, null, 4);
+
+				l.c.remove(cardSelected);
+				cardSelected = null;
+				
+			}
+			if (surface.isPressed(KeyEvent.VK_RIGHT)) {
+			
+				p.move(cardSelected, l, null, 3);
+				System.out.println("this");
+				l.c.remove(cardSelected);
+				cardSelected = null;
+				
+				
 			}
 
 			if (surface.isPressed(KeyEvent.VK_UP)) {
 				p.move(cardSelected, l, null, 1);
+				
 				l.c.remove(cardSelected);
 				cardSelected = null;
+				
 
 			}
 			if (surface.isPressed(KeyEvent.VK_DOWN)) {
+				
 				p.move(cardSelected, l, null, 2);
 				l.c.remove(cardSelected);
 				cardSelected = null;
+				
 			}
 			
 		}
 		if (p.isCleared()) {
-			isCleared = true;
+			
 			cleared[level] = true;
 		}
 		if (!p.isCleared() && cardMenu.size() == 0) {
@@ -151,6 +216,15 @@ public class Board extends Screen {
 				if (cardSelected == null) {
 					System.out.println("?");
 				}
+
+			}
+
+		}for (int i = 0; i < l.p.size(); i++) {
+
+			if (powerUpMenu.get(i).contains(p)) {
+				powerupSelected = l.p.get(i);
+				System.out.println("hit");
+				
 
 			}
 
