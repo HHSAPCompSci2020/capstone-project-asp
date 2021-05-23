6357cab7401f58ef7333b980158093d7af7588ef
Prawat967 <65191555+Prawat967@users.noreply.github.com>
2021-05-22 18:39:43 -0700

    implemented buttons into howtoplay

--- MiniGolf/src/golf/game/HowToPlay.java
+++ MiniGolf/src/golf/game/HowToPlay.java
@@ -13,7 +13,7 @@ public class HowToPlay extends Screen {
 	private DrawingSurface surface;
 	private Rectangle back;
 	private PImage backbutton, howtocards,howtocontrol,howtopowerups, howtotiles;
-	
+	private PImage controlinstruct, powerupinstruct, tileinstruct, cardinstruct;
 	boolean card,control,powerup,tile;
 	Rectangle cardbutton, controlsbutton,powerupsbutton, tilesbutton;
 	
@@ -27,6 +27,11 @@ public class HowToPlay extends Screen {
 		howtocontrol = surface.loadImage("Assets//HowToPlayControls.png");
 		howtopowerups = surface.loadImage("Assets//HowToPlayPowerUps.png");
 		howtotiles = surface.loadImage("Assets//HowToPlayTiles.png");
+		
+		controlinstruct = surface.loadImage("Assets//controlbutton.png");
+		tileinstruct = surface.loadImage("Assets//tilebutton.png");
+		powerupinstruct = surface.loadImage("Assets//powerupbutton.png");
+		cardinstruct = surface.loadImage("Assets//cardbutton.png");
 		howtotiles.resize(700, 700);
 		howtopowerups.resize(700, 700);
 		howtocards.resize(700, 700);
@@ -44,10 +49,11 @@ public class HowToPlay extends Screen {
 
 		back = new Rectangle(0, 0, 100, 100);
 		cardbutton = new Rectangle(300,100,200,100);
-		controlsbutton = new Rectangle(300,200,200,100);
-		powerupsbutton = new Rectangle(300,300,200,100);
-		tilesbutton = new Rectangle(300,400,200,100);
-			
+		controlsbutton = new Rectangle(300,250,200,100);
+		powerupsbutton = new Rectangle(300,400,200,100);
+		tilesbutton = new Rectangle(300,550,200,100);
+		
+		
 		card = false;
 		control = false;
 		
@@ -66,12 +72,15 @@ public class HowToPlay extends Screen {
 		surface.background(255, 255, 235);
 		if(!card && !powerup && !tile && !control) {
 			
-			surface.rect(cardbutton.x, cardbutton.y, cardbutton.width, cardbutton.height);
+			surface.image(cardinstruct, 300, 100);
+			surface.image(controlinstruct, 300, 250);
+			surface.image(powerupinstruct, 300, 400);
+			surface.image(tileinstruct, 300, 550);
 			
-			surface.rect(controlsbutton.x, controlsbutton.y, controlsbutton.width, controlsbutton.height);
-			surface.rect(powerupsbutton.x, powerupsbutton.y, powerupsbutton.width, powerupsbutton.height);
-			surface.rect(tilesbutton.x, tilesbutton.y, tilesbutton.width,tilesbutton.height);
 			
+			
+			
+		
 			
 			
 		}
